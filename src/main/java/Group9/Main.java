deddc8f240bcdc3506eaffe7d5a647fc4bea17a0
Michal Jarski <michal@jarski.eu>
2020-06-21 14:46:17 +0200

    Update for testing on our agents

--- src/main/java/Group9/Main.java
+++ src/main/java/Group9/Main.java
@@ -2,18 +2,19 @@ package Group9;
 
 import Group2.GreedyIntruderAgent;
 import Group9.agent.factories.DefaultAgentFactory;
+import Group9.agent.factories.OurAgentFactory;
 import Group9.map.parser.Parser;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        double x = 1;
-        double y = 1;
+        double x = 0;
+        double y = 0;
         double sum = 0;
         final double NUMBER_OF_RUNS = 50;
         for(int i=1; i <= NUMBER_OF_RUNS; i++) {
-            Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/test_2.map"), new DefaultAgentFactory(), false);
+            Game game = new Game(Parser.parseFile("./src/main/java/Group9/map/maps/test_2.map"), new OurAgentFactory(), false);
             game.run();
             System.out.printf("The winner is: %s\n", game.getWinner());
             if(game.getWinner() == Game.Team.GUARDS){
